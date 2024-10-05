84bb2e6f5c1ec06d6569bc755d7d54f1b2c08197
Bahasuru <p.h.d.bahasuru@gmail.com>
2024-10-05 21:12:35 +0530

    add tab

---
+++ js/ui.js
@@ -0,0 +1,32 @@
+const menutoggle = document.getElementById('menu-toggle');
+const menuitems = document.getElementsByClassName('menu__item');
+
+for (let i = 0; i < menuitems.length; i++) {
+    menuitems[i].addEventListener('click', function() {
+        // Code to uncheck the checkbox or perform action
+        if (menutoggle.checked) {
+            menutoggle.checked = false;
+        }
+    });
+}
+
+
+const tabs = document.querySelector(".tabs");
+const btns = document.querySelectorAll(".tbutton");
+const articles = document.querySelectorAll(".tcontent");
+tabs.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("live");
+    });
+    e.target.classList.add("live");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("live");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("live");
+  }
+});
