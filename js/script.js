79eca4e26f55e6909138c1f93e77711a49eec240
Bahasuru <p.h.d.bahasuru@gmail.com>
2025-01-26 15:19:19 +0530

    update

--- js/script.js
+++ js/script.js
@@ -354,11 +354,23 @@ function updatePlaylist() {
     listItemMap.clear();
     files.forEach((file, index) => {
         const listItem = document.createElement('li');
-        listItem.textContent = file.name;
+        const listItemtext = document.createElement('p');
+        listItemtext.textContent = file.name;
+        listItemtext.textContent = listItemtext.textContent.replace(/_/g, " ");
         listItem.setAttribute('data-index', index);
 
         const removeButton = document.createElement('button');
-        removeButton.textContent = 'Remove';
+        removeButton.classList.add('remove');
+        const removeButtonspan = document.createElement('span');
+        removeButtonspan.classList.add('removespan');
+        removeButtonspan.textContent = 'Remove';
+        const removeButtonicon = document.createElement('span');
+        removeButtonicon.classList.add('removeicon');
+        removeButtonicon.innerHTML =`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg>`;
+        
+        removeButton.appendChild(removeButtonspan);
+        removeButton.appendChild(removeButtonicon);
+        listItem.appendChild(listItemtext);
         listItem.appendChild(removeButton);
 
         listItem.addEventListener('click', () => playFile(index));
