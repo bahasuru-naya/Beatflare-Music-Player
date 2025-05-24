208cfa3bc21e6b3bcc03e81c563b25f20cab5c59
Bahasuru <p.h.d.bahasuru@gmail.com>
2025-05-25 00:50:42 +0530

    ui update

--- js/script.js
+++ js/script.js
@@ -642,15 +642,10 @@ function updatePlaylist() {
 
         const removeButton = document.createElement('button');
         removeButton.classList.add('remove');
-        const removeButtonspan = document.createElement('span');
-        removeButtonspan.classList.add('removespan');
-        removeButtonspan.textContent = 'Remove';
-        const removeButtonicon = document.createElement('span');
-        removeButtonicon.classList.add('removeicon');
-        removeButtonicon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"></path></svg>`;
+        removeButton.innerHTML = `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18 17.94 6M18 18 6.06 6"/>
+        </svg>`;
 
-        removeButton.appendChild(removeButtonspan);
-        removeButton.appendChild(removeButtonicon);
         listItem.appendChild(removeButton);
 
         listItem.addEventListener('click', () => playFile(index));
